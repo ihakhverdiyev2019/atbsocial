c10a00ae7c68d454061a5874e9e14f0a7a021e01
ihakhverdiyev2019 <ihakhverdiyev@mexxplus.com>
2021-09-14 16:56:11 +0400

    Update NewsServiceImpl.java

--- src/main/java/atb/social/network/service/News/NewsServiceImpl.java
+++ src/main/java/atb/social/network/service/News/NewsServiceImpl.java
@@ -17,12 +17,12 @@ public class NewsServiceImpl implements NewsService {
 
     @Override
     public List<NewsModel> getAllNews(int count,int page) throws Exception {
-        List<NewsModel> newsModels;
         List<NewsModel> newsModelsFilter=new ArrayList<>();
 
         try{
-            newsModels = newsRepository.findAll();
-            for(int i = (page-1)*count;i<page*count;i++){
+            List<NewsModel> newsModels = newsRepository.findAll();
+
+            for(int i = (page-1)*count ; i<page*count ; i++){
 
                 newsModelsFilter.add(newsModels.get(i));
 
