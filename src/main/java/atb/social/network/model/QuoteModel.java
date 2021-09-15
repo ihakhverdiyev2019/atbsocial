92cbb3784023a7f77a2ad1e74b3f09e0c3e0b047
ihakhverdiyev2019 <ihakhverdiyev@mexxplus.com>
2021-09-15 10:37:26 +0400

    Update QuoteModel.java

--- src/main/java/atb/social/network/model/QuoteModel.java
+++ src/main/java/atb/social/network/model/QuoteModel.java
@@ -10,8 +10,7 @@ public class QuoteModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
-    private String id;
+    private int id;
 
     private String text;
 
@@ -20,11 +19,11 @@ public class QuoteModel {
     public QuoteModel() {
     }
 
-    public String getId() {
+    public int getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(int id) {
         this.id = id;
     }
 
